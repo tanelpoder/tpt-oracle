075277ac702b33cc46d0de1cbef4a51cd7f7270a
Tanel Poder <tanel@tanelpoder.com>
2018-10-29 01:40:27 -0400

    format sql text width

--- awr/awr_sqlid.sql
+++ awr/awr_sqlid.sql
@@ -1,6 +1,8 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
+COL sql_text FOR A200 WORD_WRAP
+
 SELECT
     *
 FROM
