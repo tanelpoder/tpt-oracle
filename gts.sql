8b01c0bbf01406b2e1c5a0d4e66ad695c7ef214d
Tanel Poder <tanel@tanelpoder.com>
2019-03-22 13:17:13 -0400

    use NULL instead of USER as schema name, as NULL will honor current_schema

--- gts.sql
+++ gts.sql
@@ -2,5 +2,5 @@
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
 prompt Gather Table Statistics for table &1....
-exec dbms_stats.gather_table_stats(user, upper('&1'), null, method_opt=>'FOR TABLE FOR ALL COLUMNS SIZE REPEAT', cascade=>true, no_invalidate=>false);
+exec dbms_stats.gather_table_stats(null, upper('&1'), null, method_opt=>'FOR TABLE FOR ALL COLUMNS SIZE REPEAT', cascade=>true, no_invalidate=>false);
 
