bca09ac416af64c45f08658511a15648360c13e0
Tanel Poder <tanel@tanelpoder.com>
2019-06-03 09:51:52 -0400

    formatting

--- uu.sql
+++ uu.sql
@@ -9,13 +9,13 @@ col u_machine head MACHINE for a25 truncate
 col u_program head PROGRAM for a20
 
 select s.username u_username, ' ''' || s.sid || ',' || s.serial# || '''' u_sid, 
+       p.spid, 
+       s.sql_id, --s.sql_hash_value, 
        s.audsid u_audsid,
        s.osuser u_osuser, 
        substr(s.machine,instr(s.machine,'\')) u_machine, 
        substr(s.program,instr(s.program,'(')) u_program, 
-       p.spid, 
        -- s.sql_address, 
-       s.sql_id, --s.sql_hash_value, 
        s.last_call_et lastcall, 
        s.status 
        --, s.logon_time
