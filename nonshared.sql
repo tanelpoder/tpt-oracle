e7fad68ec90e902b46e8b6fcd6500903d1981a8f
Tanel Poder <tanel@tanelpoder.com>
2019-06-03 09:51:29 -0400

    formatting

--- nonshared.sql
+++ nonshared.sql
@@ -18,7 +18,6 @@
 --
 --------------------------------------------------------------------------------
 
---@@saveset
 set serverout on size 1000000
 prompt Show why existing SQL child cursors were not reused (V$SQL_SHARED_CURSOR)...
 prompt
@@ -39,39 +38,23 @@ declare
         l_status := dbms_sql.execute(l_theCursor);
     END;
 begin
-    execute_immediate( 'alter session set nls_date_format=
-                        ''dd-mon-yyyy hh24:mi:ss'' ');
-    dbms_sql.parse(  l_theCursor,
-                     replace( '&cmd', '"', ''''),
-                     dbms_sql.native );
-    dbms_sql.describe_columns( l_theCursor,
-                               l_colCnt, l_descTbl );
+    dbms_sql.parse(  l_theCursor, replace( '&cmd', '"', ''''), dbms_sql.native );
+    dbms_sql.describe_columns( l_theCursor, l_colCnt, l_descTbl );
     for i in 1 .. l_colCnt loop
-        dbms_sql.define_column( l_theCursor, i,
-                                l_columnValue, 4000 );
+        dbms_sql.define_column( l_theCursor, i, l_columnValue, 4000 );
     end loop;
     l_status := dbms_sql.execute(l_theCursor);
     while ( dbms_sql.fetch_rows(l_theCursor) > 0 ) loop
         for i in 1 .. l_colCnt loop
-            dbms_sql.column_value( l_theCursor, i,
-                                   l_columnValue );
+            dbms_sql.column_value( l_theCursor, i, l_columnValue );
             if l_columnValue != 'N' then 
-	            dbms_output.put_line
-	                ( rpad( l_descTbl(i).col_name,
-	                  30 ) || ': ' || l_columnValue );
+	            dbms_output.put_line ( rpad( l_descTbl(i).col_name, 30 ) || ': ' || l_columnValue );
 	        end if;
         end loop;
         dbms_output.put_line( '-----------------' );
     end loop;
-    execute_immediate( 'alter session set nls_date_format=
-                           ''dd-MON-yy'' ');
-exception
-    when others then
-        execute_immediate( 'alter session set
-                         nls_date_format=''dd-MON-yy'' ');
-        raise;
 end;
 /
 
---@@loadset
 set serverout off
+
