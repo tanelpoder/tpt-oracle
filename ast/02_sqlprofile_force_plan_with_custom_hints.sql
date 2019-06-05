9ed2151658350337b148c44336cfd4b4a8733c27
Tanel Poder <tanel@tanelpoder.com>
2019-06-05 10:33:55 -0400

    add swingbench tpcds Q31 skip scan issue

--- ast/02_sqlprofile_force_plan_with_custom_hints.sql
+++ ast/02_sqlprofile_force_plan_with_custom_hints.sql
@@ -21,11 +21,23 @@ EXEC DBMS_SQLTUNE.IMPORT_SQL_PROFILE(sql_text=>:sql_fulltext, profile=>sys.sqlpr
 -- Example 3: Set multiple hints:
 DECLARE
     hints sys.sqlprof_attr := sys.sqlprof_attr(
-        ('LEADING(@"SEL$1" "CO"@"SEL$1" "CH"@"SEL$1"')
+        ('LEADING(@"SEL$1" "CO"@"SEL$1" "CH"@"SEL$1")')
       , ('OPT_ESTIMATE(@"SEL$1", TABLE, "CU"@"SEL$1", SCALE_ROWS=100000)')
     );
 BEGIN
     DBMS_SQLTUNE.IMPORT_SQL_PROFILE(sql_text=>:sql_fulltext, profile=> hints, name=> 'MANUAL_PROFILE_1ka5g0kh4h6pc');
+END;
+/
+
+-- Example 4: SwingBench TPCDS-Like Query 31 skip scan issue (force match)
+
+DECLARE
+    hints sys.sqlprof_attr := sys.sqlprof_attr(
+        ('INDEX_SS(@"SEL$26CA4453" "STORE_SALES"@"SEL$1")')
+      , ('INDEX_SS(@"SEL$2C2C13D8" "WEB_SALES"@"SEL$2")')
+    );
+BEGIN
+    DBMS_SQLTUNE.IMPORT_SQL_PROFILE(sql_text=>:sql_fulltext, profile=> hints, name=> 'QUERY31_DISABLE_SKIP_SCAN', force_match=> TRUE);
 END;
 /
 
