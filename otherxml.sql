b50d690ae837c7163d985ebf7d159167d5103c75
Tanel Poder <tanel@tanelpoder.com>
2019-10-26 15:48:57 -0400

    xmltable passing instead of extractvalue

--- otherxml.sql
+++ otherxml.sql
@@ -5,31 +5,12 @@ select
 --      sql_id
 --    , child_number
 --    , plan_hash_value
-     substr(extractvalue(value(d), '/hint'), 1, 100) as outline_hints
+    xmltype(other_xml)
 from
-      xmltable('/*/outline_data/hint'
-        passing (
-           select
-            xmltype(other_xml) as xmlval
-           from
-            v$sql_plan
-           where
-            sql_id = '&1'
-           and  child_number like '&2' 
-           and  other_xml is not null
-        )
-) d
+    v$sql_plan
+where
+    sql_id = '&1'
+and  child_number = TO_NUMBER('&2')
+and  other_xml is not null
 /
-
---select regexp_substr(other_xml,'<!\[CDATA\[.*?\]\]>') from v$sql_plan where sql_id = '&1' and child_number like '&2';
-
--- SELECT /*+ opt_param('parallel_execution_enabled', 'false') */
---    SUBSTR(EXTRACTVALUE(VALUE(d), '/hint'), 1, 4000) hint
--- FROM
---     v$sql_plan p
---   , TABLE(XMLSEQUENCE(EXTRACT(XMLTYPE(p.other_xml), '/*/outline_data/hint'))) d
--- where 
---     sql_id = '&1' 
--- and child_number like '&2'
--- /
 
