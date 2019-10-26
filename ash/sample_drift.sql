daac6ddec05d71e766c9bb0ac6a698720083fc1e
Tanel Poder <tanel@tanelpoder.com>
2019-10-26 16:09:38 -0400

    summarize deltas

--- ash/sample_drift.sql
+++ ash/sample_drift.sql
@@ -5,7 +5,9 @@
 -- it makes sense to run this only on active systems where every sample there
 -- are some active sessions seen
 
-select to_char(sample_time,'YYYYMMDD HH24:MI'), sample_time-lag(sample_time) over(order by sample_time) 
+select 
+    to_char(sample_time,'YYYYMMDD HH24:MI')
+  , sample_time-lag(sample_time) over(order by sample_time) delta
 from (select distinct sample_time from v$active_session_history)
 /
 
