a116b636293fc6eec2e83811027db513694abc1c
Tanel Poder <tanel@tanelpoder.com>
2019-10-26 15:44:32 -0400

    formatting

--- sysm.sql
+++ sysm.sql
@@ -2,17 +2,18 @@
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
 COL sysm_group_name HEAD METRICGROUP FOR A30
+COL sysm_metric_name HEAD METRIC_NAME FOR A45
 
 prompt Display SYSTEM metrics from V$METRIC
 
 select
-   (sysdate-end_time)*86400 seconds_ago
+   (sysdate-end_time)*86400    seconds_ago
 -- , end_time last_updated
  , ROUND(mg.interval_size/100) seconds
- , m.metric_name
- , ROUND(m.value,2) value
+ , m.metric_name               sysm_metric_name
+ , ROUND(m.value,2)            value
  , m.metric_unit
- , mg.name sysm_group_name
+ , mg.name                     sysm_group_name
 from
    v$metric m
  , v$metricgroup mg
