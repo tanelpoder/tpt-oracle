f6a8f3bb4e5275eaa944d29d3eb11bba217db0e0
Tanel Poder <tanel@tanelpoder.com>
2019-10-26 15:45:38 -0400

    display sql_id col

--- sqlh.sql
+++ sqlh.sql
@@ -1,4 +1,4 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
-select sql_text from v$sql where hash_value = &1;
+select sql_id, sql_text from v$sql where hash_value = &1;
