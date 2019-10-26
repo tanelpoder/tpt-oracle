b07eae367f7729e7f80b9a7ca11c9276e33bd1b2
Tanel Poder <tanel@tanelpoder.com>
2019-10-26 15:43:32 -0400

    add comment "ACTIVE"

--- wrkasum.sql
+++ wrkasum.sql
@@ -1,7 +1,7 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
--- Purpose:     Show a summary of SQL workareas grouped by operation type (SORT, HASH, etc)
+-- Purpose:     Show a summary of ACTIVE SQL workareas grouped by operation type (SORT, HASH, etc)
 --              Show both PGA workarea usage and TEMP usage by workareas (workareas don't include
 --              other PGA/TEMP use like PL/SQL arrays and global temporary tables)
 --
