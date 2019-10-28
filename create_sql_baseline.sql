b69a20ee9293b57fbb3d161ebcffb23e7f55dfbf
Tanel Poder <tanel@tanelpoder.com>
2019-10-28 01:32:28 -0400

    add comments about 18c+ SE usage

--- create_sql_baseline.sql
+++ create_sql_baseline.sql
@@ -15,7 +15,11 @@
 -- You can get the SQL_HANDLE from DBA_SQL_PLAN_BASELINES
 --
 -- More info:
---    SQL Plan Baselines - DBMS_SPM in EE licenses - see http://jonathanlewis.wordpress.com/2011/01/12/fake-baselines/
+--    SQL Plan Baselines - DBMS_SPM in EE licenses 
+--      - https://jonathanlewis.wordpress.com/2011/01/12/fake-baselines/
+--    DBMS_SPM basic baseline use is included in Oracle EE in 11g+ and SE from 18c+
+--      - https://blogs.oracle.com/optimizer/does-the-use-of-sql-plan-management-and-the-dbmsspm-database-package-require-a-tuning-or-diagnostic-pack-license
+--
 --------------------------------------------------------------------------------------------------------
 SET SERVEROUT ON SIZE 1000000
 
--- drop_sql_baseline.sql
+++ drop_sql_baseline.sql
@@ -4,6 +4,11 @@ DEF sql_handle=&1
 -- as this is what DBMS_XPLAN reports as used... (so no manual lookup from plan_name -> sql_handle
 -- is needed)
 
+-- You can get the SQL_HANDLE from DBA_SQL_PLAN_BASELINES
+
+-- DBMS_SPM basic baseline use is included in Oracle EE in 11g+ and SE from 18c+
+--   https://blogs.oracle.com/optimizer/does-the-use-of-sql-plan-management-and-the-dbmsspm-database-package-require-a-tuning-or-diagnostic-pack-license
+
 SET SERVEROUT ON SIZE 1000000
 
 DECLARE
