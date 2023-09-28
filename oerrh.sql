fbea3e3a1c9a2b6bda484928110d838e28a5ed12
tanel <tanel@tanelpoder.com>
2023-09-28 18:04:23 -0400

    v1

---
+++ oerrh.sql
@@ -0,0 +1,6 @@
+-- Copyright 2023 Tanel Poder. All rights reserved. More info at https://tanelpoder.com
+-- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms and conditions.
+
+-- The _start variable below is defined in init.sql (Win: start, OSX: open, Linux: xdg-open
+PROMPT Opening https://docs.oracle.com/en/error-help/db/ora-&1
+HOST &_start https://docs.oracle.com/en/error-help/db/ora-&1
