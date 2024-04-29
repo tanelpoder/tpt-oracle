c958171f2a56e603b9ba87b9caea05856f1c322b
Tanel Poder <tanel@tanelpoder.com>
2024-04-29 13:33:54 -0400

    replace sample_id based walking with truncated timestamp for RAC sample_id drift reasons

--- ash/ash_wait_chains.sql
+++ ash/ash_wait_chains.sql
@@ -3,7 +3,7 @@
 
 --------------------------------------------------------------------------------
 --
--- File name:   ash_wait_chains.sql (v0.6 BETA)
+-- File name:   ash_wait_chains.sql (v0.8)
 -- Purpose:     Display ASH wait chains (multi-session wait signature, a session
 --              waiting for another session etc.)
 --              
@@ -17,7 +17,7 @@
 --     @ash_wait_chains username||':'||program2||event2 session_type='FOREGROUND' sysdate-1/24 sysdate
 --
 -- Other:
---     This script uses only the in-memory V$ACTIVE_SESSION_HISTORY, use
+--     This script uses only the in-memory G$ACTIVE_SESSION_HISTORY, use
 --     @dash_wait_chains.sql for accessiong the DBA_HIST_ACTIVE_SESS_HISTORY archive
 --
 --     Oracle 10g does not  have the BLOCKING_INST_ID column in ASH so you'll need
@@ -26,16 +26,16 @@
 --              
 --------------------------------------------------------------------------------
 COL wait_chain FOR A300 WORD_WRAP
-COL distinct_sids FOR 9999 HEAD "#Blkrs"
 COL "%This" FOR A6
 
 PROMPT
-PROMPT -- Display ASH Wait Chain Signatures script v0.6 BETA by Tanel Poder ( http://blog.tanelpoder.com )
+PROMPT -- Display ASH Wait Chain Signatures script v0.8 by Tanel Poder ( https://tanelpoder.com )
 
 WITH 
 bclass AS (SELECT /*+ INLINE */ class, ROWNUM r from v$waitstat),
 ash AS (SELECT /*+ INLINE QB_NAME(ash) LEADING(a) USE_HASH(u) SWAP_JOIN_INPUTS(u) */
             a.*
+          , CAST(a.sample_time AS DATE) sample_time_s -- round timestamp to 1 sec boundary for matching across RAC nodes
           , o.*
           , u.username
           , CASE WHEN a.session_type = 'BACKGROUND' AND a.program LIKE '%(DBW%)' THEN
@@ -89,11 +89,11 @@ ash AS (SELECT /*+ INLINE QB_NAME(ash) LEADING(a) USE_HASH(u) SWAP_JOIN_INPUTS(u
         AND a.current_obj# = o.object_id(+)
         AND sample_time BETWEEN &3 AND &4
     ),
-ash_samples AS (SELECT /*+ INLINE */ DISTINCT sample_id FROM ash),
+ash_samples AS (SELECT /*+ INLINE */ DISTINCT sample_time_s FROM ash),
 ash_data AS (SELECT /*+ INLINE */ * FROM ash),
 chains AS (
     SELECT /*+ INLINE */ 
-        sample_time ts
+        d.sample_time_s ts
       , level lvl
       , session_id sid
       , REPLACE(SYS_CONNECT_BY_PATH(&1, '->'), '->', ' -> ')||CASE WHEN CONNECT_BY_ISLEAF = 1 AND d.blocking_session IS NOT NULL THEN ' -> [idle blocker '||d.blocking_inst_id||','||d.blocking_session||','||d.blocking_session_serial#||(SELECT ' ('||s.program||')' FROM gv$session s WHERE (s.inst_id, s.sid , s.serial#) = ((d.blocking_inst_id,d.blocking_session,d.blocking_session_serial#)))||']' ELSE NULL END path -- there's a reason why I'm doing this 
@@ -108,12 +108,12 @@ chains AS (
         ash_samples s
       , ash_data d
     WHERE
-        s.sample_id = d.sample_id 
+        s.sample_time_s = d.sample_time_s
     AND d.sample_time BETWEEN &3 AND &4
     CONNECT BY NOCYCLE
         (    PRIOR d.blocking_session = d.session_id
          AND PRIOR d.blocking_inst_id = d.inst_id
-         AND PRIOR s.sample_id = d.sample_id
+         AND PRIOR s.sample_time_s = d.sample_time_s -- Different RAC nodes have sample_id drift (assuming that clocks are synced enough)
         )
     START WITH &2
 )
@@ -122,12 +122,12 @@ SELECT * FROM (
         LPAD(ROUND(RATIO_TO_REPORT(COUNT(*)) OVER () * 100)||'%',5,' ') "%This"
       , COUNT(*) seconds
       , ROUND(COUNT(*) / ((CAST(&4 AS DATE) - CAST(&3 AS DATE)) * 86400), 1) AAS
-      , COUNT(DISTINCT sids) distinct_sids
       , path wait_chain
       , TO_CHAR(MIN(sample_time), 'YYYY-MM-DD HH24:MI:SS') first_seen
       , TO_CHAR(MAX(sample_time), 'YYYY-MM-DD HH24:MI:SS') last_seen
-      -- , MIN(sids)
-      -- , MAX(sids)
+      , COUNT(DISTINCT sids)
+      , MIN(sids) -- show a couple of ultimate blocker sids
+      , MAX(sids)
     FROM
         chains
     WHERE
