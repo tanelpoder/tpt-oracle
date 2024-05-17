b7b4e4a4b63eedee1615b634ae0ddb4e68b23d94
patrickjolliffe <jolliffe@gmail.com>
2024-05-17 11:43:17 +0100

    f.sql also search view name

--- f.sql
+++ f.sql
@@ -1,12 +1,26 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
+
+-- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
+-- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
+
+--------------------------------------------------------------------------------
+--
+-- File name:   f.sql
+-- Purpose:     Search for Fixed view (V$ view) text
+--
+-- Author:      Tanel Poder
+-- Copyright:   (c) http://www.tanelpoder.com
+--              
 -- Usage:       @f <text>
 --              @f sql_shared
+--
+--------------------------------------------------------------------------------
 
 col view_name for a25 wrap
 col text for a100 word_wrap
 
-prompt Search for Fixed view (V$ view) text containing %&1%
+prompt Search for Fixed view (V$ view) with view name or text containing %&1%
 
-select view_name, view_definition text from v$fixed_View_definition where upper(view_definition) like upper('%&1%');
+select view_name, view_definition text from v$fixed_View_definition where upper(view_name) like upper('%&1%') or upper(view_definition) like upper('%&1%');
 
