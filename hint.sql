9d3235f6be247d7c6ec84b5b84935a990f1996cc
tanel <tanel@tanelpoder.com>
2024-06-04 05:16:02 -0400

    optimizer hing scope display

--- hint.sql
+++ hint.sql
@@ -4,18 +4,19 @@ col name for a35
 col version for a15
 col version_outline for a15
 col inverse for a35
-col statement_level for a17
-col query_block_level for a17
-col object_level for a17
-col join_level for a17
+
+col hint_scope for a20
+
 select 
     name,
     version,
     version_outline,
     inverse,
-    decode(bitand(target_level,1),0,'NO','YES') AS statement_level,
-    decode(bitand(target_level,2),0,'NO','YES') AS query_block_level,
-    decode(bitand(target_level,4),0,'NO','YES') AS object_level,
-    decode(bitand(target_level,6),0,'NO','YES') AS join_level
+    decode(bitand(target_level,1),0,'','STATEMENT ') ||
+    decode(bitand(target_level,2),0,'','QUERY_BLOCK ')  ||
+    decode(bitand(target_level,4),0,'','OBJECT ')  ||
+    decode(bitand(target_level,6),0,'','JOIN ') hint_scope
 from v$sql_hint 
-where lower(name) like lower('%&1%');
+where lower(name) like lower('%&1%')
+/
+
