1d02e97c49e1786785b915f87117a38bed424c21
Tanel Poder <tanel@tanelpoder.com>
2024-06-05 03:16:02 -0400

    early version change sql formatting

--- hint.sql
+++ hint.sql
@@ -1,4 +1,6 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
-select name,version,version_outline,inverse from v$sql_hint where lower(name) like lower('%&1%');
+select name,version,version_outline,inverse 
+from v$sql_hint 
+where lower(name) like lower('%&1%') escape '\';
