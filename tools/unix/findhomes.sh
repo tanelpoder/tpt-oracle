d19e024478ebc0d815a53de40b5ea3c62e581967
Nikola Ivanov <nikola.ivanov@dskbank.bg>
2024-12-09 17:27:09 +0200

    Edited findhomes.sh

--- tools/unix/findhomes.sh
+++ tools/unix/findhomes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A little helper script for finding ORACLE_HOMEs for all running instances in a Linux server
-# # # by Tanel Poder (http://blog.tanelpoder.com)
-# #
+# by Tanel Poder (http://blog.tanelpoder.com)
+
 printf "%6s %-20s %-80s\n" "PID" "NAME" "ORACLE_HOME"
 ps -Ao pid,cmd | grep _pmon_ | grep -v grep | while read pid pname; do
 	printf "%6s %-20s %-80s\n" $pid $pname `readlink /proc/$pid/exe | sed 's/bin\/oracle$//' | sort | uniq`
