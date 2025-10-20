7dc973cbe208f399341dcfe19e4a6a05d0c9d056
tanel <tanel@tanelpoder.com>
2025-10-20 02:01:26 -0400

    simplify output formatting

--- tabhisthybrid.sql
+++ tabhisthybrid.sql
@@ -12,13 +12,14 @@
 --   I'm hoping to unify this script and my old tabhist.sql to show estimated cardinalities
 --   (for equality filters) for all histogram types and data types someday.
 
+COL phys_bucket            HEAD BUCKET FOR 99999
 COL table_owner            FORMAT A15
 COL table_name             FORMAT A30
 COL column_name            FORMAT A30
 COL data_type              HEAD DATA_TYPE FORMAT A12
-COL histogram_type         FORMAT A14
+COL histogram_type         HEAD HIST_TYPE FORMAT A12
 COL endpoint_number        FORMAT 9999999999
-COL endpoint_value         FORMAT 9999999999
+COL endpoint_value         HEAD LOOKUP_VALUE FORMAT 9999999999
 COL endpoint_actual_value  FORMAT A40
 COL estimated_rows         FORMAT 9999999999
 COL endpoint_repeat_count  FORMAT 999999
@@ -92,7 +93,8 @@ nd AS (
         bkt
 )
 SELECT
-    data_type                          AS data_type
+    rownum                             AS phys_bucket
+  , data_type                          AS data_type
   , histogram                          AS histogram_type
   , endpoint_value                     AS endpoint_value
   , CASE
@@ -113,9 +115,9 @@ SELECT
             -1 
         ELSE
             NULL
-    END                                AS card_thispop
-  , TRUNC(c.num_rows * nd.newdensity)  AS card_nonpop
-  , NVL(endpoint_repeat_count,0)       AS endpoint_repeat_count
+    END                                AS card_thisval
+  , TRUNC(c.num_rows * nd.newdensity)  AS card_non_pop
+--  , NVL(endpoint_repeat_count,0)       AS endpoint_repeat_count
 --  , endpoint_number                    AS endpoint_number
 --  , endpoint_actual_value              AS endpoint_actual_value
 --  , olddensity                         AS olddensity
