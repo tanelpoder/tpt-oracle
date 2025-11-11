14223ac8bc780ca4beb4df6d8df911a507846193
Tanel Poder <tanel@tanelpoder.com>
2025-11-10 21:30:36 -0500

    format log size in MB

--- log.sql
+++ log.sql
@@ -5,6 +5,24 @@ prompt Show redo log layout from V$LOG, V$STANDBY_LOG and V$LOGFILE...
 
 col log_member head MEMBER for a100
 
-select * from v$log order by group#;
+select
+    GROUP#       
+  , THREAD#      
+  , SEQUENCE#    
+  , ROUND(BYTES/1024/1024) SIZE_MB     
+  , BLOCKSIZE    
+  , MEMBERS      
+  , ARCHIVED     
+  , STATUS       
+  , FIRST_CHANGE#
+  , FIRST_TIME   
+  , NEXT_CHANGE# 
+  , NEXT_TIME    
+from
+    v$log
+order by
+    group#
+/
+
 select * from v$standby_log order by group#;
 select group#, status, type, is_recovery_dest_file, member log_member from v$logfile order by group#,member;
