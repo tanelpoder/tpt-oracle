2e1aae566f33d819796496cb3dcc2e1a21a1016b
Tanel Poder <tanel@tanelpoder.com>
2026-05-15 00:46:31 -0400

    avoid wide output

--- pdbs.sql
+++ pdbs.sql
@@ -1,4 +1,7 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
-SELECT * FROM v$pdbs;
+COL pdb_name FOR A30
+
+SELECT con_id, dbid, name pdb_name, open_mode FROM v$pdbs;
+
