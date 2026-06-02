b1e56f532571bc68703d2ba204aaac2b24a51dcf
Tanel Poder <tanel@tanelpoder.com>
2026-06-02 00:48:48 -0400

    early version of reporting Other Info ("binoculars") of SQL Mon plans

---
+++ sqlmon_info.sql
@@ -0,0 +1,74 @@
+-- Copyright 2025 Tanel Poder. All rights reserved. More info at https://tanelpoder.com
+-- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
+
+-- File:  sqlmon_info.sql
+-- Usage: @sqlmon_info <SQL_ID> 
+--
+-- NOTES:
+--   1) This is an early draft of the final script
+--   2) Currently it reports the SQL_IDs monitored execution that has STARTED most recently
+--   3) You have to switch to the appropriate PDB to see monitored queries inside a PDB
+--
+-- DOP downgrade_reasons FROM X$QKSXA_REASON:
+-- 
+--  352: DOP downgrade due to adaptive DOP
+--  353: DOP downgrade due to resource manager max DOP
+--  354: DOP downgrade due to insufficient number of processes
+--  355: DOP downgrade because PX servers failed to join
+
+COL sqlmon_plan_op FOR A50
+COL plan_line_id HEAD ID FOR 9999
+
+SELECT
+--    s.sql_id
+--  , s.status
+--  , s.sql_exec_start
+    p.plan_line_id 
+  , LPAD(' ', p.plan_depth, ' ') || p.plan_operation || ' ' || p.plan_options sqlmon_plan_op
+--  , s.px_is_cross_instance
+--  , s.px_maxdop           
+--  , s.px_maxdop_instances 
+  , s.px_servers_requested px_req
+  , s.px_servers_allocated px_alloc
+--  , s.key
+--  , s.sid
+--  , p.sid
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_1_id)) otherstat_1 
+  , p.otherstat_1_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_2_id)) otherstat_2 
+  , p.otherstat_2_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_3_id)) otherstat_3 
+  , p.otherstat_3_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_4_id)) otherstat_4 
+  , p.otherstat_4_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_5_id)) otherstat_5 
+  , p.otherstat_5_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_6_id)) otherstat_6 
+  , p.otherstat_6_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_7_id)) otherstat_7 
+  , p.otherstat_7_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_8_id)) otherstat_8 
+  , p.otherstat_8_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_9_id)) otherstat_9 
+  , p.otherstat_9_value 
+  , (SELECT name FROM v$sql_monitor_statname WHERE (group_id,id) = (p.otherstat_group_id,p.otherstat_10_id)) otherstat_10
+  , p.otherstat_10_value
+-- FYI Oracle 26ai has 30 otherstats columns...
+FROM
+    gv$sql_monitor      s
+  , gv$sql_plan_monitor p
+WHERE
+-- join
+    s.inst_id = p.inst_id
+AND s.con_id = p.con_id
+AND s.key = p.key
+-- filter
+AND s.con_id = SYS_CONTEXT('userenv', 'con_id')
+AND s.sql_id = '&1'
+AND s.px_qcsid IS NULL -- QC for PX queries (or serial)
+AND s.sql_exec_start = (SELECT MAX(sql_exec_start) FROM gv$sql_monitor WHERE sql_id='&1')
+ORDER BY
+    s.key
+  , p.plan_line_id
+/
+
